f0ccfcf606949f1bafceea35363c79f7a4d193d0
Ayush5071 <ayusht5071@gmail.com>
2024-06-04 18:05:18 +0530

    Implemented code for joiining the room in both client and server side.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import { Server, Socket } from "socket.io";
 
-const io = new Server(8000);
+const io = new Server(8000,{
+    cors: true,
+});
+
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
 
 io.on('connection',socket=>{
     console.log('Socket Connected',socket.id)
+    socket.on('room:join',data => {
+        const {email,room} = data;
+        emailToSocketIdMap.set(email,socket.id);
+        socketIdToEmailMap.set(socket.id,email);
+        io.to(socket.id).emit('room:join',data);
+    })
 });
 
